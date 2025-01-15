df904dee04e018a2192b82d1eff2498e35e85892
syplaning <syplaning@gmail.com>
2025-01-15 14:34:43 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,66 @@
+  <footer>
+    <div class="footer_inner">
+      <div class="btn_wrap">
+        <div class="qlink">
+          <a href="app.html">
+            <img src="1x/qlink.png" alt="사이트바로가기" />
+          </a>
+        </div>
+        <div class="top">
+          <a href="#">
+            <img src="1x/top1.png" alt="gotop" />
+          </a>
+        </div>
+      </div>
+      <div class="f_left">
+        <img src="1x/logo.png" alt="에이블씨엔티" />
+        <div class="f_info">
+          <p><span class="m">CEO </span>우동주</p>
+          <p>
+            <span class="m">본사 </span>부산광역시 강서구 과학산단1로 60번길 2B동
+            207호
+          </p>
+          <p>
+            <span class="m">기술연구소 </span>부산광역시 강서구 과학산단1로 60번길 30
+            7동 601호
+          </p>
+          <div class="contact">
+            <p><span class="m">TEL </span> 051-123-456</p>
+            <p><span class="m">FAX </span> 051-123-456</p>
+            <p><span class="m">E-MAIL </span> loenkorea@loen.com</p>
+          </div>
+        </div>
+      </div>
+      <div class="f_right">
+        <ul>
+          <li>
+            <h5>COMPANY</h5>
+            <a href="sub1_1.html">CEO 인사말</a>
+            <a href="sub1_2.html">사업분야 · 비전</a>
+            <!-- <a href="sub1_3.html" >회사 비전</a> -->
+            <a href="sub1_4.html">오시는길</a>
+          </li>
+
+          <li>
+            <h5>SERVICE</h5>
+            <a href="#" onclick="alert('준비중입니다.'); return false;">사업수행 실적</a>
+          </li>
+
+          <li>
+            <h5>PRODUCT</h5>
+            <a href="sub3_1.html">제품소개</a>
+            <a href="app.html">안전발걸음</a>
+          </li>
+
+          <li>
+            <h5>CONTACT</h5>
+            <a href="sub4.html">고객센터</a>
+            <!-- <a href="#" onclick="alert('ready'); return false;">협력문의</a> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="copy">
+      <p>Copyrights © ABLECNT Co., Ltd. All rights reserved.</p>
+    </div>
+  </footer>
---
+++ header.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>로엔코리아</title>
+
+  <meta name="title" content="로엔코리아" />
+  <meta name="description" content="미래를 생각하는 SMART SOLUTION기업" />
+  <meta name="keywords" content="로엔코리아, 로엔, 안전, 아이길, 스마트 자동화, 신재생에너지, 안전발걸음, 디바이스,">
+  <!-- Open Graph -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="" />
+  <meta property="og:title" content="로엔코리아" />
+  <meta property="og:description" content="미래를 생각하는 SMART SOLUTION기업" />
+
+  <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/sub.css" />
+  <link rel="stylesheet" as="style" crossorigin
+    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+  <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+</head>
+
+<body>
+  <header>
+    <div class="burgur_menu_wrap">
+      <div class="menu_wrap">
+        <div class="">
+          <p>COMPANY</p>
+          <a href="sub1_1.html" class="">CEO 인사말</a>
+          <a href="sub1_2.html" class="">사업분야 · 비전</a>
+          <!-- <a href="sub1_3.html" class="">회사 비전</a> -->
+          <a href="sub1_4.html" class="">오시는 길</a>
+        </div>
+        <div class="">
+          <p onclick="alert('준비중입니다.'); return false;">SERVICE</p>
+          <a href="sub2_1.html" class="" onclick="alert('준비중입니다.'); return false;">사업수행 실적</a>
+        </div>
+        <div class="">
+          <p>PRODUCT</p>
+          <a href="sub3_1.html" class="">제품소개</a>
+          <a href="app.html" class="">안전발걸음</a>
+        </div>
+        <div class="">
+          <p>CONTACT</p>
+          <a href="sub4.html" class="">고객센터</a>
+          <!-- <a href="sub4_2.html" class="" onclick="alert('ready'); return false;"
+       >협력문의</a
+      > -->
+        </div>
+      </div>
+    </div>
+    <div class="header_wrap">
+      <h1>
+        <a href="index.html"><img src="1x/logo.png" alt="ABLECNT" /></a>
+      </h1>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="sub1_1.html">COMPANY</a>
+            <ul class="depth">
+              <li><a href="sub1_1.html">CEO 인사말</a></li>
+              <li><a href="sub1_2.html">사업분야 · 비전</a></li>
+              <!-- <li><a href="sub1_3.html">회사 비전</a></li> -->
+              <li><a href="sub1_4.html">오시는 길</a></li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="sub2_1.html" onclick="alert('준비중입니다'); return false;">SERVICE</a>
+            <ul class="depth">
+              <li>
+                <a href="sub2_1.html" onclick="alert('준비중입니다'); return false;">사업수행 실적</a>
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="sub3_1.html">PRODUCT</a>
+            <ul class="depth">
+              <li><a href="sub3_1.html">제품소개</a></li>
+              <li><a href="app.html">안전발걸음</a></li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="sub4.html">CONTACT</a>
+            <ul class="depth">
+              <li>
+                <a href="sub4.html">고객센터</a>
+              </li>
+              <!-- <li>
+         <a href="sub4_2.html" onclick="alert('ready'); return false;"
+          >협력문의</a
+         >
+        </li> -->
+            </ul>
+          </li>
+        </ul>
+      </nav>
+
+      <div class="loen">
+        <a href="https://artmazedsample.cafe24.com/roem/" target="_blank">
+          <img src="1x/qlinklogo.png" alt="사이트바로가기" />
+        </a>
+      </div>
+
+      <div class="r_menu">
+        <input type="checkbox" id="ham-toggle" hidden="" />
+        <label for="ham-toggle" class="ham">
+          <span class="line"></span>
+          <span class="line"></span>
+        </label>
+      </div>
+    </div>
+  </header>
