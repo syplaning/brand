db0c99af81a86afc62819e55001ffe523cc2e1aa
syplaning <syplaning@gmail.com>
2025-01-15 14:35:34 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+   const ham = document.querySelector(".ham");
+   const siteMenu = document.querySelector(".burgur_menu_wrap");
+   ham.addEventListener("click", function () {
+    siteMenu.classList.toggle("view");
+   });
+// site map
+
+
+
+//  sub3_1 tab
+
+
